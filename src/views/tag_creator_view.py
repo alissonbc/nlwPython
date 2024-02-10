b227c894ad92ca0ddf9df80145caf9872fe44f08
José Alisson <josealissonif@gmail.com>
2024-02-10 14:27:25 -0300

    code python formatted for extension

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,15 +1,18 @@
+from src.controller.tag_creator_controller import TagCreatorController
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
-from src.controller.tag_creator_controller import TagCreatorController
+
+
 class TagCreatorView:
-    '''
+    """
     responsability for interacting with HTTP
-    '''
+    """
+
     def validade_and_create(self, http_request: HttpRequest):
         tag_creator_controller = TagCreatorController()
         body = http_request.body
-        product_code = body.get('product_code')
-        
+        product_code = body.get("product_code")
+
         formatted_response = tag_creator_controller.create(product_code)
 
         return HttpResponse(status_code=200, body=formatted_response)
