b0f5d549060b41a8892b47eb2e9670e554f5ac19
José Alisson <josealissonif@gmail.com>
2024-02-10 05:50:25 -0300

    lesson end 2

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,12 +1,15 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controller.tag_creator_controller import TagCreatorController
 class TagCreatorView:
     '''
     responsability for interacting with HTTP
     '''
     def validade_and_create(self, http_request: HttpRequest):
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body.get('product_code')
-        print('estou na minha View')
+        
+        formatted_response = tag_creator_controller.create(product_code)
 
-        return HttpResponse(status_code=200, body={'hello': 'world'})
+        return HttpResponse(status_code=200, body=formatted_response)
